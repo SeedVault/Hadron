7c1c37cb7b09ead6f2ec4a0444f01aca47a9da5b
John <31740633+johnmbotanic@users.noreply.github.com>
2018-07-23 19:39:36 -0500

    Delete config.beta.js

--- src/config.beta.js
+++
@@ -1,9 +0,0 @@
-/*jshint esversion: 6 */
-
-export default {
-  also_known_as                  : "beta",
-  all_your_bases_are_belong_to_us: "./",
-  mtone_base_uri                 : "https://mtone-beta.botanic.io/api/v41/",
-  author_tool_domain             : "https://system-beta.botanic.io",
-  api_key                        : "bbc37f6e1382c5f8fbb9397e960152c2"
-};
